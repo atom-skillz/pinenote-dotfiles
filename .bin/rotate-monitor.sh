2e0db0248a7bbb948f220a540fceff74e09bcb80
atom_skillz <atom.skillz@protonmail.com>
2025-04-28 11:41:30 -0600

    Add touchscreen

--- .bin/rotate-monitor.sh
+++ .bin/rotate-monitor.sh
@@ -5,6 +5,8 @@ if [[ -z $1 ]]; then
     declare -i t=$(hyprctl monitors DPI-1 | grep transform | cut -d " " -f 2)
     declare -i nt=(t+1)%4
 fi
+declare -i tt=(nt+2)%4
 
-echo transform $nt
+echo transform $nt $tt
 hyprctl keyword monitor DPI-1,transform,$nt
+hyprctl keyword input:touchdevice:transform $tt
